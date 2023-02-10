35529b70d32603e199294e9f4fc6887ddfaa8d7d
jatolentino <jose_antoniotv@hotmail.com>
2023-02-10 17:56:18 -0500

    RedisConfig.java changed

---
+++ src/main/java/com/application/bidding/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.application.bidding.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Change this to "localhost" if running from local
+        // Change this to "redis" if running from docker container
+        return new LettuceConnectionFactory("redis", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
