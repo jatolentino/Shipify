dc3b978e37046d7d8f4adf579e884b400c10a711
jatolentino <jose_antoniotv@hotmail.com>
2023-02-12 06:15:04 -0500

    AdminController.java fixed

---
+++ src/main/java/com/application/bidding/controller/AdminController.java
@@ -0,0 +1,41 @@
+package com.application.auction.controller;
+
+import com.application.auction.model.Admin;
+import com.application.auction.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @GetMapping("/admins")
+    public @ResponseBody Set<Admin> getAllAdmins(){
+        return adminService.getAllAdmins();
+    }
+
+    @GetMapping("/admins/{adminId}")
+    public @ResponseBody Admin getAdmin(@PathVariable Long adminId){
+        return adminService.getAdmin(adminId);
+    }
+
+    @PostMapping("/admins")
+    public @ResponseBody Admin addAdmin(@RequestBody Admin newAdmin){
+        return adminService.addAdmin(newAdmin);
+    }
+
+    @PutMapping("/admins")
+    public @ResponseBody Admin updateAdmin(@RequestBody Admin updatedAdmin){
+        return adminService.updateAdmin(updatedAdmin);
+    }
+
+    @DeleteMapping("/admins/{adminId}")
+    public void deleteAdmin(@PathVariable Long adminId){
+        adminService.deleteAdmin(adminId);
+    }
+}
