c8c2e295eaae172486d13c09db269004a76993f7
jatolentino <jose_antoniotv@hotmail.com>
2023-02-16 19:11:22 -0500

    CategoryController.java updated

---
+++ src/main/java/com/application/bidding/controller/CategoryController.java
@@ -0,0 +1,46 @@
+package com.application.auction.controller;
+
+import com.application.auction.model.Category;
+import com.application.auction.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/categories/")
+    public @ResponseBody Set<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/categories/{categoryId}")
+    public @ResponseBody Category getCategory(@PathVariable Long categoryId){
+        return categoryService.getCategory(categoryId);
+    }
+
+    @PostMapping("/categories/")
+    public @ResponseBody Category addCategory(@RequestBody Category newCategory){
+        return categoryService.addCategory(newCategory);
+    }
+
+    @PutMapping("/categories/")
+    public @ResponseBody Category updateCategory(@RequestBody Category updatedCategory){
+        return categoryService.updateCategory(updatedCategory);
+    }
+
+    @DeleteMapping("/categories/{categoryId}")
+    public void deleteCategory(@PathVariable Long categoryId){
+        categoryService.deleteCategory(categoryId);
+    }
+
+    @GetMapping("/categories/{categoryId}/items")
+    public @ResponseBody Set<Long> getItemsByCategory(@PathVariable Long categoryId){
+        return categoryService.getItemsByCategory(categoryId);
+    }
+}
