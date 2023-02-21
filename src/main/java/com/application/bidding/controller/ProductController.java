d4dc93999381a2bb850d902d33b78662ad80817f
jatolentino <jose_antoniotv@hotmail.com>
2023-02-21 08:07:40 -0500

    ProductController.java added

---
+++ src/main/java/com/application/bidding/controller/ProductController.java
@@ -0,0 +1,88 @@
+package com.application.bidding.controller;
+
+import com.application.bidding.model.Product;
+import com.application.bidding.repository.IProductRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private IProductRepository iProductRepository;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok().body(iProductRepository.findAll());
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") String id) {
+        Optional<Product> productOptional = iProductRepository.findById(new ObjectId(id));
+
+        return productOptional.isPresent() ?
+                new ResponseEntity<>(productOptional.get(), HttpStatus.FOUND) :
+                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        try {
+            Product _product = iProductRepository.save(new Product(
+                    product.getName(),
+                    product.getDescription(),
+                    product.isPublished(),
+                    product.getImageURL()
+            ));
+            return new ResponseEntity<>(_product, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+        Optional<Product> productOptional = iProductRepository.findById(new ObjectId(id));
+
+        if (!productOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Product _product = productOptional.get();
+        _product.setId(product.getId());
+        _product.setName(product.getName());
+        _product.setDescription(product.getDescription());
+        _product.setPublished(product.isPublished());
+        _product.setImageURL(product.getImageURL());
+
+        return new ResponseEntity<>(iProductRepository.save(_product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") String id) {
+        try {
+            iProductRepository.deleteById(new ObjectId(id));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<HttpStatus> deleteAllProducts() {
+        try {
+            iProductRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
