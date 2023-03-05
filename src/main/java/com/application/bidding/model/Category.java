99e54fad616e09d05bd4d4a1b7daa2429d958905
jatolentino <jose_antoniotv@hotmail.com>
2023-03-05 10:37:48 -0500

    Category.java fixed

---
+++ src/main/java/com/application/bidding/model/Category.java
@@ -0,0 +1,66 @@
+package com.application.auction.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "categoryId")
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long categoryId;
+
+    @Column(unique = true, nullable = false)
+    private String categoryName;
+
+    @ManyToMany(targetEntity = Item.class)
+    private Set<Item> items;
+
+    protected Category(){}
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public void setItem(Item item){
+        if (this.items == null){
+            this.items = new HashSet<>();
+        }
+        this.items.add(item);
+    }
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
