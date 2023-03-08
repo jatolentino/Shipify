336cd13d514c0ff339fa61c2e5306d883ade6054
jatolentino <jose_antoniotv@hotmail.com>
2023-03-08 11:15:20 -0500

    Product.java replaced

---
+++ src/main/java/com/application/bidding/model/Product.java
@@ -0,0 +1,39 @@
+package com.application.bidding.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "Product")
+public class Product {
+
+    @Id
+    private ObjectId id;
+    private String name;
+    private String description;
+    private boolean published;
+    private String imageURL;
+
+    public Product(String name, String description, boolean published, String imageURL) {
+        this.id = new ObjectId();
+        this.name = name;
+        this.description = description;
+        this.published = published;
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", published=" + published +
+                ", imageURL='" + imageURL + '\'' +
+                '}';
+    }
+}
