5117e35d0fd63e33f3de8a117d86542212ed6c26
jatolentino <jose_antoniotv@hotmail.com>
2023-03-11 11:52:52 -0500

    State.java modified

---
+++ src/main/java/com/application/bidding/model/State.java
@@ -0,0 +1,64 @@
+package com.application.auction.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "stateId")
+public class State {
+
+    @Id
+    @GeneratedValue
+    private int stateId;
+
+    @Column(unique = true, nullable = false)
+    private String stateName;
+
+    @OneToMany(mappedBy = "itemState", targetEntity = Item.class)
+    private Set<Item> items;
+
+    protected State(){}
+
+    public State(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public State(int stateId, String stateName) {
+        this.stateId = stateId;
+        this.stateName = stateName;
+    }
+
+    public State(int stateId, String stateName, Set<Item> items) {
+        this.stateId = stateId;
+        this.stateName = stateName;
+        this.items = items;
+    }
+
+    public Set<Item> getItem() {
+        return items;
+    }
+
+    public void setItem(Set<Item> items) {
+        this.items = items;
+    }
+
+    public int getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(int stateId) {
+        this.stateId = stateId;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+}
