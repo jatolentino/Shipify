e39a2e71e73713bbc1557c09edfa7ebf1fd5e2fa
jatolentino <jose_antoniotv@hotmail.com>
2023-03-14 12:30:24 -0500

    User.java fixed

---
+++ src/main/java/com/application/bidding/model/User.java
@@ -0,0 +1,39 @@
+package com.application.bidding.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "User")
+public class User {
+
+    @Id
+    private ObjectId id;
+    private String name;
+    private String surname;
+    private String mail;
+    private String password;
+
+    public User(String name, String surname, String mail, String password) {
+        this.id = new ObjectId();
+        this.name = name;
+        this.surname = surname;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", mail='" + mail + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
