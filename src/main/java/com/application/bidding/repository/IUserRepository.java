655f014221522b597760b33e2fd3c1d2324f429d
jatolentino <jose_antoniotv@hotmail.com>
2023-03-25 02:41:46 -0500

    IUserRepository.java changed

---
+++ src/main/java/com/application/bidding/repository/IUserRepository.java
@@ -0,0 +1,17 @@
+package com.application.bidding.repository;
+
+import com.application.bidding.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends MongoRepository<User, String> {
+    // Define custom finder methods here
+    User findByMail(String mail);
+    User findByMailAndPassword(String mail, String password);
+    Optional<User> findById(ObjectId objectId);
+    void deleteById(ObjectId objectId);
+}
