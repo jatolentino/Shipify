bbd1a95ace2e5eec668cdfabd5e0d546bbbc144c
jatolentino <jose_antoniotv@hotmail.com>
2023-03-26 15:00:32 -0500

    ItemRepository.java replaced

---
+++ src/main/java/com/application/bidding/repository/ItemRepository.java
@@ -0,0 +1,13 @@
+package com.application.auction.repository;
+
+import com.application.auction.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("SELECT i FROM Item i WHERE upper(i.itemName) LIKE CONCAT('%', upper(:queryString), '%')")
+    List<Item> findByitemName(String queryString);
+}
