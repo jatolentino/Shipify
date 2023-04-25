c5e764dfea3fd422005606f4040b3c0f982983a4
jatolentino <jose_antoniotv@hotmail.com>
2023-04-25 08:07:40 -0500

    ItemService.java updated

---
+++ src/main/java/com/application/bidding/service/ItemService.java
@@ -0,0 +1,64 @@
+package com.application.auction.service;
+
+import com.application.auction.model.Auction;
+import com.application.auction.model.Item;
+import com.application.auction.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getAllItems(){
+//        List<Item> items = new ArrayList<>();
+//        itemRepository.findAll().forEach(items::add);
+//        return items;
+        return itemRepository.findAll();
+    }
+
+    public Set<Long> getAllItemIds(){
+        Set<Long> itemIds = new HashSet<>();
+        List<Item> items = getAllItems();;
+
+        for (Item item : items){
+            itemIds.add(item.getItemId());
+        }
+
+        return itemIds;
+    }
+
+    public Item addItem(Item newItem){
+        return itemRepository.save(newItem);
+    }
+
+    public Item updateItem(Item updatedItem){
+        return itemRepository.save(updatedItem);
+    }
+
+    public void deleteItem(Long itemId){
+        itemRepository.deleteById(itemId);
+    }
+
+    public Item getItem(Long itemId){
+        return itemRepository.findById(itemId).orElse(null);
+    }
+
+    public Set<Long> searchItem(String queryString) {
+        List<Item> items = itemRepository.findByitemName(queryString);
+        Set<Long> itemIds = new HashSet<>();
+        for (Item item : items){
+            itemIds.add(item.getItemId());
+        }
+        return itemIds;
+    }
+}
