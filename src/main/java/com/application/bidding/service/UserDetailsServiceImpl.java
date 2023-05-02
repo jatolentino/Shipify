f99d7fcb99bb143a54a2fd866c3d457bbec08b29
jatolentino <jose_antoniotv@hotmail.com>
2023-05-02 05:37:32 -0500

    UserDetailsServiceImpl.java added

---
+++ src/main/java/com/application/bidding/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.application.auction.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+import com.application.auction.model.User;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        User user = userService.findbyEmail(userEmail);
+        if (user == null) {
+            throw new UsernameNotFoundException(userEmail);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUserEmail(),
+                user.getUserPassword(), emptyList());
+    }
+}
