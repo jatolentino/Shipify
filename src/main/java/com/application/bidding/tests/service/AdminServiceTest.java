4794083557c488793cacd53a3eea0d2e41b2db33
jatolentino <jose_antoniotv@hotmail.com>
2023-05-05 06:15:04 -0500

    AdminServiceTest.java created

---
+++ src/main/java/com/application/bidding/tests/service/AdminServiceTest.java
@@ -0,0 +1,24 @@
+package com.application.auction.service;
+
+import com.application.auction.model.Admin;
+import com.application.auction.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
