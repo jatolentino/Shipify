2508743f4bb81ac214f52e629cb3f81182c084ec
jatolentino <jose_antoniotv@hotmail.com>
2023-05-09 19:11:22 -0500

    CategoryServiceTest.java created

---
+++ src/main/java/com/application/bidding/tests/service/CategoryServiceTest.java
@@ -0,0 +1,23 @@
+package com.application.auction.service;
+
+import com.application.auction.model.Category;
+import com.application.auction.model.Item;
+import com.application.auction.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
