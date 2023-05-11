151f7b8645d808d2d42650618419d49aebab0adb
jatolentino <jose_antoniotv@hotmail.com>
2023-05-11 07:30:08 -0500

    ItemServiceTest.java changed

---
+++ src/main/java/com/application/bidding/tests/service/ItemServiceTest.java
@@ -0,0 +1,25 @@
+package com.application.auction.service;
+
+import com.application.auction.model.Auction;
+import com.application.auction.model.Item;
+import com.application.auction.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
