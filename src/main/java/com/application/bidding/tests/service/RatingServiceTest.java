b811345e95382e1fb6ab4cd3443d5e0afcd4d569
jatolentino <jose_antoniotv@hotmail.com>
2023-05-12 19:48:54 -0500

    RatingServiceTest.java created

---
+++ src/main/java/com/application/bidding/tests/service/RatingServiceTest.java
@@ -0,0 +1,25 @@
+package com.application.auction.service;
+
+import com.application.auction.Holder.ItemRating;
+import com.application.auction.Holder.UserRating;
+import com.application.auction.model.Auction;
+import com.application.auction.model.Rating;
+import com.application.auction.repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
