ae8c058a66d38efafa6f40d9ff9b22ebf49eecca
jatolentino <jose_antoniotv@hotmail.com>
2023-05-15 20:26:26 -0500

    StateServiceTest.java fixed

---
+++ src/main/java/com/application/bidding/tests/service/StateServiceTest.java
@@ -0,0 +1,22 @@
+package com.application.auction.service;
+
+import com.application.auction.model.State;
+import com.application.auction.repository.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateServiceTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
