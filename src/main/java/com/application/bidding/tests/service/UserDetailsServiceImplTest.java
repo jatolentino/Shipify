b3c69609244997732de69fefd2c3a63ca9b01348
jatolentino <jose_antoniotv@hotmail.com>
2023-05-17 08:45:12 -0500

    UserDetailsServiceImplTest.java modified

---
+++ src/main/java/com/application/bidding/tests/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.application.auction.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+import com.application.auction.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDetailsServiceTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
