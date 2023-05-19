aceb7b9ab6338817283fb1726c518495ac8cc782
jatolentino <jose_antoniotv@hotmail.com>
2023-05-18 21:03:58 -0500

    UserServiceTest.java added

---
+++ src/main/java/com/application/bidding/tests/service/UserServiceTest.java
@@ -0,0 +1,23 @@
+package com.application.auction.service;
+
+import com.application.auction.model.Auction;
+import com.application.auction.model.Item;
+import com.application.auction.model.User;
+import com.application.auction.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
