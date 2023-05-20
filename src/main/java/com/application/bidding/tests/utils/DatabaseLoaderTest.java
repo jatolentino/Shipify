dcee461ca5d71b6f52a7c16d7055e3af0aca9db4
jatolentino <jose_antoniotv@hotmail.com>
2023-05-20 09:22:44 -0500

    DatabaseLoaderTest.java added

---
+++ src/main/java/com/application/bidding/tests/utils/DatabaseLoaderTest.java
@@ -0,0 +1,23 @@
+package com.application.auction.Util;
+
+import com.application.auction.model.*;
+import com.application.auction.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConverterTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
