fd428d451e6e7ccca23a93c8890dd27e2aa5d751
jatolentino <jose_antoniotv@hotmail.com>
2023-05-21 21:41:30 -0500

    DateConverterTest.java modified

---
+++ src/main/java/com/application/bidding/tests/utils/DateConverterTest.java
@@ -0,0 +1,16 @@
+package com.application.bidding.utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConverterTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
