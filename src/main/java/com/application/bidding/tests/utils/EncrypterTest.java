98f458a9967d2ac6599239587ddc792c7adab1a1
jatolentino <jose_antoniotv@hotmail.com>
2023-05-23 10:00:16 -0500

    EncrypterTest.java fixed

---
+++ src/main/java/com/application/bidding/tests/utils/EncrypterTest.java
@@ -0,0 +1,15 @@
+package com.application.bidding.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncrypterTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
