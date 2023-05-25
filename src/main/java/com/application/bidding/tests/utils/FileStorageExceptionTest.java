ad0acbf307805dd8900f3668898837fbcf61141a
jatolentino <jose_antoniotv@hotmail.com>
2023-05-24 22:19:02 -0500

    FileStorageExceptionTest.java updated

---
+++ src/main/java/com/application/bidding/tests/utils/FileStorageExceptionTest.java
@@ -0,0 +1,12 @@
+package com.application.auction.Util;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSotrageExceptionTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
