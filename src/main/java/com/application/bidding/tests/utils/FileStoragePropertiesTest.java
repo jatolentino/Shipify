4c35b402712bc4670bfc51736d3468169c290c6b
jatolentino <jose_antoniotv@hotmail.com>
2023-05-27 05:00:00 -0500

    FileStoragePropertiesTest.java updated

---
+++ src/main/java/com/application/bidding/tests/utils/FileStoragePropertiesTest.java
@@ -0,0 +1,15 @@
+package com.application.auction.Util;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoragePropertiesTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
