dad2139bdb2a5f98c517b02f2208f609dd6b8915
jatolentino <jose_antoniotv@hotmail.com>
2023-05-30 05:37:32 -0500

    MyFileNotFoundExceptionTest.java added

---
+++ src/main/java/com/application/bidding/tests/utils/MyFileNotFoundExceptionTest.java
@@ -0,0 +1,15 @@
+package com.application.auction.Util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFileNotFoundExceptionTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
