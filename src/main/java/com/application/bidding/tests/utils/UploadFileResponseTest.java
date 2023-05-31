26d94bd3513ec88a783c572f5d3f277cfa6282db
jatolentino <jose_antoniotv@hotmail.com>
2023-05-31 17:56:18 -0500

    UploadFileResponseTest.java fixed

---
+++ src/main/java/com/application/bidding/tests/utils/UploadFileResponseTest.java
@@ -0,0 +1,14 @@
+package com.application.auction.Util;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileResponseTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
