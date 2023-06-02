56c15743af10e86dae495bce02c380dc76982469
jatolentino <jose_antoniotv@hotmail.com>
2023-06-02 06:15:04 -0500

    UserAlreadyExistsExceptionTest.java updated

---
+++ src/main/java/com/application/bidding/tests/utils/UserAlreadyExistsExceptionTest.java
@@ -0,0 +1,14 @@
+package com.application.auction.Util;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAlreadyExistExceptionTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
