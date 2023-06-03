314988f28b7af3f839ea664070033bfff52b2621
jatolentino <jose_antoniotv@hotmail.com>
2023-06-03 18:33:50 -0500

    WebSocketTest.java fixed

---
+++ src/main/java/com/application/bidding/tests/utils/WebSocketTest.java
@@ -0,0 +1,18 @@
+package com.application.bidding.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketTests {
+
+    @Test
+    void contestLoads(){
+
+    }
+}
