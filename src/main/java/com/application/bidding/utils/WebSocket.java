086cb36bbee169389530551fd0afe1ca77ab4f3e
jatolentino <jose_antoniotv@hotmail.com>
2023-06-19 17:18:46 -0500

    WebSocket.java replaced

---
+++ src/main/java/com/application/bidding/utils/WebSocket.java
@@ -0,0 +1,23 @@
+package com.application.bidding.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocket implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/bid");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:8080").withSockJS();
+    }
+}
